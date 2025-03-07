955cb297b8647e907fb5dc8b7314bcefe096588f
D4niel-dv <daniel.p.araujo@iesb.edu.br>
2025-03-07 19:27:00 -0300

    MATEMATICA

---
+++ AULA 03/revisao.js
@@ -0,0 +1,37 @@
+console.log("TESTE")
+
+const nome = " GUSTAVO"
+let idade = 20
+const MaiorDeIdade = true 
+
+console.log (nome)
+console.log (idade)
+console.log (" é maior de idade?",MaiorDeIdade )
+
+
+const soma = 2+2
+const subtracao = 2-2
+const multiplicacao = 2*2
+const divisao = 4 / 2
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+
+const resto = 3 % 2
+let numero = 4
+const incremento = numero++
+const decremento = numero--
+
+console.log ("RESTO",resto)
+console.log ("INCREMENTO",incremento)
+console.log ("NUMERO",numero)
+console.log ("DECREMENTO",decremento)
+
+let a = 3
+
+a %= 2
+
+console.log(a)
+
