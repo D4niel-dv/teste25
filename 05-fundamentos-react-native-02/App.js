e0cd59217bd8b245c4f08e8909e2f9754d1db628
D4niel-dv <daniel.p.araujo@iesb.edu.br>
2025-03-28 19:22:32 -0300

    atualizando o git hub

---
+++ 05-fundamentos-react-native-02/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import PrimeiroComponente from './componentes/PrimeiroComponente';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <PrimeiroComponente></PrimeiroComponente>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
